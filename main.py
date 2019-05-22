0c3935fea559136da8ffe73896c6121c0b3bc9a3
George <george.ridgway@protonmail.com>
2019-05-22 09:41:58 -0500

    removed more python code

--- main.py
+++
@@ -1,6 +0,0 @@
-from Controller.ControllerLogic import ControllerLogic
-
-if __name__ == '__main__':
-    cachedController = ControllerLogic()
-    ControllerLogic.start_application(cachedController)
-
